3f49327ba0836c36756a926fba740973e7155982
yeonuk99 <144973782+yeonuk99@users.noreply.github.com>
2023-11-15 18:39:37 +0900

    Add files via upload

---
+++ CheckForm.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project
+{
+    public partial class CheckForm : Form
+    {
+        public CheckForm()
+        {
+            InitializeComponent();
+
+
+
+        }
+
+        private void btn_Yes_Click(object sender, EventArgs e)
+        {
+            ModalForm.Reselectrd_Player++;
+            ModalForm.Position_num = 0;
+            Close();
+        }
+
+        private void btn_No_Click(object sender, EventArgs e)
+        {
+
+            
+            Close();
+
+        }
+
+
+    }
+}
